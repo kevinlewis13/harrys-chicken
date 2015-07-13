f0e126f480e2d89d4fe16223cd516bf48677672b
Kevin Lewis <kevinlewis13@gmail.com>
2015-07-12 20:24:15 -0700

    adds environment files

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+  // we want nodemon server restart
+  // scss to css conversion
+  // webpack build
+  // watch for html,css,js, jsx file changes
+  // clean build directory task
+  // minifier/uglifier
+})
