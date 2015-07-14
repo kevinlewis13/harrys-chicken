5aecad0af9d69c897ea91077f649b1b5e22d025a
Andrew Jacobson <andyjacuw@gmail.com>
2015-07-14 15:56:39 -0700

    Set up basic auth routes

--- server/routes/auth_routes.js
+++ server/routes/auth_routes.js
@@ -1,1 +1,22 @@
 'use strict';
+
+var bodyparser = require('body-parser');
+var Dish = require('../models/Dish');
+
+// routes will eventually require authentication
+module.exports = function(router) {
+  router.use(bodyparser.json());
+
+  router.post('/', function(req, res) {
+    var newDish = new Dish(req.body);
+
+    newDish.save(function(err, data) {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({msg: '500 Internal server error'});
+      }
+
+      res.json(data);
+    });
+  });
+};
