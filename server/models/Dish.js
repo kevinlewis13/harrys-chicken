576a526067556dc05848c49b0949d6e79b01b859
Kevin Lewis <kevinlewis13@gmail.com>
2015-07-15 11:29:39 -0700

    updates model

--- server/models/Dish.js
+++ server/models/Dish.js
@@ -4,7 +4,9 @@ var mongoose = require('mongoose');
 
 var dishSchema = mongoose.Schema({
   title: { type: String, required: true },
-  price: { type: Number, required: true }
+  price: { type: Number, required: true },
+  description: String,
+  category: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Dish', dishSchema);
