679c85d07153237ed8b1b0d724dcafe04458cb58
Andrew Jacobson <andyjacuw@gmail.com>
2015-07-15 13:19:03 -0700

    Add basic react components

--- app/js/client.jsx
+++ app/js/client.jsx
@@ -1,6 +1,15 @@
 'use strict';
 
 var React = require('react');
-var number1 = 1;
-var nm = 3;
-var number2 = 7;
+var request = require('superagent');
+var Header = require('./components/header.jsx');
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <Header/>
+    );
+  }
+});
+
+React.render(<App/>, document.body);
---
+++ app/js/components/header.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <header className="header-chicken">
+        <h1>Harry's Chicken Joint</h1>
+      </header>
+    );
+  }
+});
