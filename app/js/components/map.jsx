5f7e9f88357c55c009c05ac28dac2112ca33a2a8
Andrew Jacobson <andyjacuw@gmail.com>
2015-07-20 11:54:21 -0700

    Remove old commented code

--- app/js/components/map.jsx
+++ app/js/components/map.jsx
@@ -26,7 +26,6 @@ module.exports = React.createClass({
   componentDidMount: function() {
     this.map = this.createMap();
     this.marker = this.createMarker();
-    //this.infoWindow = this.createInfoWindow();
   },
 
   render: function() {
@@ -34,31 +33,4 @@ module.exports = React.createClass({
       <section id="map" ref="map_canvas"></section>
     );
   }
-
-  // getDefaultProps: function () {
-  //     return {
-  //         initialZoom:15,
-  //         mapCenterLat: 47.547487,
-  //         mapCenterLng: -122.386976,
-  //     };
-  // },
-  // componentDidMount: function (rootNode) {
-  //     var mapOptions = {
-  //         center: this.mapCenterLatLng(),
-  //         zoom: this.props.initialZoom
-  //     },
-  //     map = new google.maps.Map(this.getDOMNode(), mapOptions);
-  //     var marker = new google.maps.Marker({position: this.mapCenterLatLng(), title: 'Hi', map: map});
-  //     this.setState({map: map});
-  // },
-  // mapCenterLatLng: function () {
-  //     var props = this.props;
-  //     return new google.maps.LatLng(props.mapCenterLat, props.mapCenterLng);
-  // },
-  // render: function () {
-  //     return (
-  //       <section id="map" ref="map_canvas"></section>
-  //     );
-  // }
-
 });
