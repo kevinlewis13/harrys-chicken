22afec1944e89df148d0a82d1dda922976c0b78e
Andrew Jacobson <andyjacuw@gmail.com>
2015-07-23 13:02:57 -0700

    Add simple nav

--- app/js/components/header.jsx
+++ app/js/components/header.jsx
@@ -5,6 +5,12 @@ module.exports = React.createClass({
     return (
       <header className="slab header">
         <h1>Harry's Chicken Joint</h1>
+        <nav className="nav">
+          <a href="#location">Location</a>
+          <a href="#menu">Menu</a>
+          <a href="#about">About</a>
+          <a href="tel:+2069389000">206.938.9000</a>
+        </nav>
       </header>
     );
   }
