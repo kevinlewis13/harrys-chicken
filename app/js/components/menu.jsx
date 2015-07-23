cbab143dd69728ea09cc42552f0886c6aab74c77
Andrew Jacobson <andyjacuw@gmail.com>
2015-07-23 13:03:18 -0700

    Uppercase/pluralize category names

--- app/js/components/menu.jsx
+++ app/js/components/menu.jsx
@@ -6,10 +6,13 @@ module.exports = React.createClass({
   renderCategories: function() {
     var categoryGroups = this.groupByCategories();
     var categoryNames = _.keys(categoryGroups);
+    var categoryTitle;
 
     return _.map(categoryNames, function(categoryName) {
+      categoryTitle = categoryName[0].toUpperCase() + categoryName.slice(1) + 's';
+
       return (
-        <li key={categoryName}>{categoryName}
+        <li key={categoryName} className="category">{categoryTitle}
           <Category categoryDishes={categoryGroups[categoryName]}/>
         </li>
       );
