47df9f6a4206d67c634f8a58cf141256394ed6e9
Kevin Lewis <kevinlewis13@gmail.com>
2015-08-07 09:27:29 -0700

    changes model to accept string for price (trailing 0 problem), adds basic layout for form readability, deletes form comments, reorders buttons

--- server/models/Dish.js
+++ server/models/Dish.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
 var dishSchema = mongoose.Schema({
   restaurant: { type: String, required: true },
   title: { type: String, required: true },
-  price: { type: Number, required: true },
+  price: { type: String, required: true },
   description: String,
   category: { type: String, required: true },
 });
