21e9be0e17f521f996efd9ee3a6c13c200dad1a9
Kevin Lewis <kevinlewis13@gmail.com>
2015-08-11 13:51:04 -0700

    adds backend auth files, updates server

---
+++ server/lib/eat_auth.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var eat = require('eat');
+var User = require('../models/User');
+
+module.exports = function(secret) {
+  return function(req, res, next) {
+    var token = req.headers.eat || req.body.eat;
+
+    if (!token) {
+      console.log('unauthorized, no token in request');
+      return res.status(401).json({msg: 'not authorized, no token in request'});
+    }
+
+    eat.decode(token, secret, function(err, decoded) {
+      if (err) {
+        console.log(err);
+        return res.status(401).json({msg: 'not authorized, could not decode'});
+      }
+
+      User.findOne({ _id: decoded.id }, function(err, user) {
+        if (err) {
+          console.log(err);
+          return res.status(401).json({msg: 'not authorized, error finding user'});
+        }
+
+        if (!user) {
+          console.log('could not find user for that token');
+          return res.status(401)..json({msg: 'not authorized, could not find that user'});
+
+        }
+
+        req.user = user;
+        next();
+      });
+    });
+  };
+};
