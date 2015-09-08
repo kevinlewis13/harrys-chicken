369445151139ff15593cbeb74a93247fec897fdd
Andrew Jacobson <andyjacuw@gmail.com>
2015-09-08 14:04:36 -0700

    Use value to update input

--- app/js/components/admin/input.jsx
+++ app/js/components/admin/input.jsx
@@ -3,14 +3,24 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return { value: this.props.value };
+  },
+
+  handleChange: function(evt) {
+    this.setState({value: evt.target.value});
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    this.setState({value: nextProps.value});
+  },
 
   render: function() {
     return (
       <label>{this.props.labelName}
         <input required={this.props.isRequired} name={this.props.name} type="text"
-          placeholder={this.props.placeholder} defaultValue={this.props.default}></input>
+          placeholder={this.props.placeholder} value={this.state.value} onChange={this.handleChange}></input>
       </label>
     );
   }
-
 });
