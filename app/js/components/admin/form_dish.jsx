c9960e129c7c578afb7265a1e9caab166d58bba6
Andrew Jacobson <andyjacuw@gmail.com>
2015-09-08 14:03:53 -0700

    Revert to original state on cancel

--- app/js/components/admin/form_dish.jsx
+++ app/js/components/admin/form_dish.jsx
@@ -7,18 +7,24 @@ var $ = require('jquery');
 
 module.exports = React.createClass({
   getInitialState: function() {
-    return { editing: false, dish: this.props.dish };
+    return { editing: false };
   },
 
   handleToggleEdit: function(evt) {
-    $(evt.target).parent().siblings('.dish-edit-form').slideToggle('fast');
-    this.setState({editing: !this.state.editing});
+    $(evt.target)
+      .parent()
+      .siblings('.dish-edit-form')
+      .slideToggle('fast', function() {
+        this.setState({editing: !this.state.editing});
+      }.bind(this));
   },
 
   handleSubmit: function(id, evt) {
     this.props.submit(id, evt);
-    $(evt.target).slideUp('fast');
-    this.setState({editing: !this.state.editing});
+    $(evt.target)
+      .slideUp('fast', function() {
+        this.setState({editing: false});
+      }.bind(this));
   },
 
   handleDelete: function(id, evt) {
@@ -26,25 +32,24 @@ module.exports = React.createClass({
   },
 
   render: function() {
-    var index = this.state.dish.index + 1;
+    var index = this.props.dish.index + 1;
     var buttonText = this.state.editing ? 'Cancel' : 'Edit';
 
     return (
       <section className="dish-edit-section" key={this.props.dish._id}>
         <header className="dish-edit-header">
           <h4>{index}. {this.props.dish.title}</h4>
-          <button onClick={this.handleToggleEdit} className="toggle-edit-button">{buttonText}</button>
+          <button className="delete-button" onClick={this.handleDelete.bind(null, this.props.dish._id)}>Delete</button>
+          <button className="toggle-edit-button" onClick={this.handleToggleEdit}>{buttonText}</button>
         </header>
-        <form className="dish-edit-form" name="updateItem" key={this.props.dish._id}
-          onSubmit={this.handleSubmit.bind(null, this.props.dish._id)}>
-          <Input isRequired={true} labelName="Name" name="name" default={this.state.dish.title} placeholder="item name"/>
-          <Input isRequired={true} labelName="Price" name="price" default={this.state.dish.price} placeholder="item price"/>
-          <Input isRequired={false} labelName="Description" name="description" default={this.state.dish.description} placeholder="item description"/>
-          <Input isRequired={true} labelName="Menu Position" name="index" default={index} placeholder="item index"/>
-          <DropDown name="restaurant" default={this.state.dish.restaurant} options={this.props.restaurantOptions} />
-          <DropDown name="category" default={this.state.dish.category} options={this.props.categoryOptions} />
+        <form className="dish-edit-form" name="updateItem" onSubmit={this.handleSubmit.bind(null, this.props.dish._id)}>
+          <Input isRequired={true} labelName="Name" name="name" value={this.props.dish.title} placeholder="item name"/>
+          <Input isRequired={true} labelName="Price" name="price" value={this.props.dish.price} placeholder="item price"/>
+          <Input isRequired={false} labelName="Description" name="description" value={this.props.dish.description} placeholder="item description"/>
+          <Input isRequired={true} labelName="Menu Position" name="index" value={index} placeholder="item index"/>
+          <Dropdown name="restaurant" default={this.props.dish.restaurant} options={this.props.restaurantOptions}/>
+          <Dropdown name="category" default={this.props.dish.category} options={this.props.categoryOptions}/>
           <button className="save-button" type="submit">Save Changes</button>
-          <button className="delete-button" onClick={this.handleDelete.bind(null, this.props.dish._id)}>Delete Item</button>
         </form>
       </section>
     );
